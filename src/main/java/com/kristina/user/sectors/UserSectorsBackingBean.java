d5394a13a08fa9a2442a7c7b6fa20dbc4631259b
Nogteva <kristina.nogteva@cgi.com>
2022-11-05 19:59:39 +0200

    Setting up jsf, primefaces, adding basic styling

---
+++ src/main/java/com/kristina/user/sectors/UserSectorsBackingBean.java
@@ -0,0 +1,26 @@
+package com.kristina.user.sectors;
+
+//import jakarta.annotation.PostConstruct;
+
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserSectorsBackingBean {
+
+    private List<SelectItem> sectorsMenuItems;
+
+    //@PostConstruct
+    private void init(){
+        initializeSectorsMenuItems();
+    }
+
+    private void initializeSectorsMenuItems(){
+        sectorsMenuItems = new ArrayList<>();
+        // TODO initialize
+    }
+
+    public List<SelectItem> getSectorsMenuItems() {
+        return sectorsMenuItems;
+    }
+}
