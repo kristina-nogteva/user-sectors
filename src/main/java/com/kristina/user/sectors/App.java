dd64da224372c04fd43303841f3798e9b5d99804
Nogteva <kristina.nogteva@cgi.com>
2022-11-05 10:26:29 +0200

    adding maven project

---
+++ src/main/java/com/kristina/user/sectors/App.java
@@ -0,0 +1,13 @@
+package com.kristina.user.sectors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
