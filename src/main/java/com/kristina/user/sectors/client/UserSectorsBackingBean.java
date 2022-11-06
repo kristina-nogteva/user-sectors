a09f9beab01112160303c9266437bda54ceeaa19
Nogteva <kristina.nogteva@cgi.com>
2022-11-06 17:08:33 +0200

    setting up spring, hibernate, h2 database configuration

--- src/main/java/com/kristina/user/sectors/client/UserSectorsBackingBean.java
+++ src/main/java/com/kristina/user/sectors/client/UserSectorsBackingBean.java
@@ -1,10 +1,18 @@
-package com.kristina.user.sectors;
+package com.kristina.user.sectors.client;
+
+import org.apache.myfaces.orchestra.conversation.annotations.ConversationName;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.faces.model.SelectItem;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
+@Scope("conversation.access")
+@ConversationName("editActivityBackingBean")
 public class UserSectorsBackingBean {
 
     private List<SelectItem> sectorsMenuItems;
@@ -12,6 +20,11 @@ public class UserSectorsBackingBean {
     @PostConstruct
     private void init(){
         initializeSectorsMenuItems();
+    }
+
+    @Transactional
+    public void save(){
+
     }
 
     private void initializeSectorsMenuItems(){
